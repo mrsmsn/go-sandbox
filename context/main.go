21b5aec8e03f818665d33cc32fb807a4bf3261b3
mrsmsn <mrsmsn43@gmail.com>
2021-08-26 14:15:25 +0900

    Add context-1

---
+++ context/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func infiniteLoop(ctx context.Context) {
+	// 終わらないやつ
+	for {
+		fmt.Println("Help!")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go infiniteLoop(ctx)
+
+	// 2秒待ってキャンセルする
+	time.Sleep(2 * time.Second)
+	cancel()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
---
+++ context/memo.md
@@ -0,0 +1,4 @@
+# contextよくわからないから適当に動作確認してみる
+
+## ref
+[golangでcontextパッケージを使う - write ahead log](https://twinbird-htn.hatenablog.com/entry/2017/04/07/214420)
