f6569fb9dec52d5b8f08d67117a9e8d374cd9169
mrsmsn <mrsmsn43@gmail.com>
2021-11-27 17:26:28 +0900

    Add pkg-ex

---
+++ pkg-ex/helloworld/go.mod
@@ -0,0 +1,3 @@
+module hellorworld
+
+go 1.16
---
+++ pkg-ex/helloworld/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+"fmt"
+"github.com/mrsmsn/go-sandbox/pkg-ex/calculator"
+)
+
+func main() {
+    n := 3
+    n2 := 5
+	fmt.Println(calculator.Sum(n, n2))
+}
